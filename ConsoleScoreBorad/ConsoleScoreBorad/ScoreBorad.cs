ec6eb603701150012e283be3696f8093cba518e9
Patricia <53782160+Patricia-Pato@users.noreply.github.com>
2020-11-17 20:08:37 +0900

    ScoreBorad2の追加

---
+++ ConsoleScoreBorad/ConsoleScoreBorad/ScoreBorad.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleScoreBorad
+{
+    class ScoreBorad
+    {
+        public int Score1 { get; set; }
+        public int Score2 { get; set; }
+        public int Score3 { get; set; }
+        public int Score4 { get; set; }
+
+        public ScoreBoradMode Mode { get; set; }
+    }
+
+    enum ScoreBoradMode
+    {
+        ScoreBorad,
+        Announcement,
+        Clock
+    }
+}
---
+++ ScoreBorad2/ScoreBorad2/ViewModels/ScoreBoradViewModel.cs
@@ -0,0 +1,49 @@
+﻿using ScoreBorad2.Properties;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ScoreBorad2.ViewModels
+{
+    class ScoreBoradViewModel : NotificationObject
+    {
+        public ScoreBoradViewModel()
+        {
+            _block1 = new BlockViewModel(Settings.Default.BlockName1);
+            _block2 = new BlockViewModel(Settings.Default.BlockName2);
+            _block3 = new BlockViewModel(Settings.Default.BlockName3);
+            _block4 = new BlockViewModel(Settings.Default.BlockName4);
+        }
+
+        private BlockViewModel _block1;
+        public BlockViewModel Block1
+        {
+            get { return this._block1; }
+            set { SetProperty(ref this._block1, value); }
+        }
+
+        private BlockViewModel _block2;
+        public BlockViewModel Block2
+        {
+            get { return this._block2; }
+            set { SetProperty(ref this._block2, value); }
+        }
+
+        private BlockViewModel _block3;
+        public BlockViewModel Block3
+        {
+            get { return this._block3; }
+            set { SetProperty(ref this._block3, value); }
+        }
+        private BlockViewModel _block4;
+        public BlockViewModel Block4
+        {
+            get { return this._block4; }
+            set { SetProperty(ref this._block4, value); }
+        }
+
+
+    }
+}
